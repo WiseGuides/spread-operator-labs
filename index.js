cc2c0e96c54388f1b8d0da2f22725267ed64a037
Hal Helms <hal.helms@gmail.com>
2019-01-23 06:11:26 -0800

    6 exercises

---
+++ index.js
@@ -0,0 +1,34 @@
+// Spread Operator Lab
+// Rev. 22Jan2019
+
+/*
+1. Create an array, composers, with elements: Bach, Beethoven, Brahms
+*/
+
+/*
+2. Create a new array, myComposers, using the spread operator to incorporate the elements in composers -- and adding two new names: Berlioz and Britten
+*/
+
+/*
+3. Create a function, showComposer, that accepts an array of composers and outputs each one. 
+*/
+
+/*
+4. Using the code below, use the spread operator to extract the array elements of numbers and send them to greatestOfThree.
+
+let numbers = [5,2,9];
+let greatestOfThree = (num1, num2, num3) => {
+  let greatest = num1;
+  greatest = num2 > greatest && num2;
+  greatest = num3 > greatest && num3;
+  return greatest;
+};
+*/
+
+/*
+5. Create a JavaScript object -- you decide which keys and values should be included. Now, create a new object that starts with those key/value pairs by using the spread operator.
+*/
+
+/*
+6. Reverse the strings "racecar" and "deified"
+*/
